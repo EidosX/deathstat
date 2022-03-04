87742b9a4eba6ce61ec02bc69101b93514ad1eb8
Diego Imbert <diegoimbert@pm.me>
2022-03-04 21:12:52 +0100

    Added 2021 age pyramid

--- src/read_pop.py
+++ src/read_pop.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def read_pop():
-    return read_pop_1901_2020()
+    return pd.concat([read_pop_1901_2020(), read_pop_2021()], ignore_index=True)
 
 
 def read_pop_1901_2020():
@@ -28,4 +28,22 @@ def read_pop_1901_2020():
     df["year"] = df["year"].astype(int)
     df[(df["sex"] == "F") & (df["year"] == 2020)]
     df["pop"] = df["pop"].replace("-", 0).astype(int)
+    return df
+
+
+def read_pop_2021():
+    df = pd.read_excel("assets/download/age_pyramid_2021.xlsx",
+                       sheet_name="2021 Métro", skiprows=range(5))
+
+    df = df.drop(["Année de naissance", "Ensemble"], axis=1)
+    df = df.rename(columns={
+        "Âge révolu": "age",
+        "Nombre d'hommes": "M",
+        "Nombre de femmes": "F",
+    })
+    df = df[:-1]
+    df.loc[105, "age"] = 105
+    df = df.melt(id_vars=["age"], var_name="sex", value_name="pop")
+    df["year"] = 2021
+
     return df
